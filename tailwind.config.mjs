eb24b44eb279dcc596d1056e574b14d10c154125
santiagoMosquera99 <krakedev3@gmail.com>
2024-05-06 09:53:19 -0500

    Mejoras de ui al index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"darkColor": "#30313b",
+				"MoradoPastel": "#b19cd9",
+				"RosaSuave": "#f4bfbf"
+			},
+			boxShadowColor: {
+				"moradaSuave": "0 30px 50px #b19cd977"
+			}
+		},
 	},
 	plugins: [],
 }
